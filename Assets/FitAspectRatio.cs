62337ea1c598c8b69a3d4bc4ec3b739b85215da7
Nicklas Frederiksen <nfrederiksen@hotmail.com>
2017-01-18 13:06:32 +0100

    Startup - No Scene

---
+++ Assets/FitAspectRatio.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FitAspectRatio : MonoBehaviour {
+
+	[SerializeField] GameObject slopeBlock;
+
+	void Start() {
+		float newWidth = slopeBlock.transform.localScale.x * Screen.height / Screen.width * 0.5f;
+
+		Camera.main.orthographicSize = newWidth;
+		//gameObject.transform.localScale = new Vector3(width, 1f, width);
+
+	}
+}
---
+++ Assets/SlopeController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlopeController : MonoBehaviour {
+
+	public GameObject player;
+	public GameObject snowSlope;
+	public GameObject snowSlopeWrapper;
+
+	void Update() {
+		float checkSlope = player.transform.position.z + snowSlope.transform.localScale.z;
+		if (!Physics.CheckSphere(new Vector3(player.transform.position.x, player.transform.position.y, checkSlope), 1f)) {
+			Debug.Log("No Slope");
+			GameObject slope = (GameObject) Instantiate(snowSlope, snowSlopeWrapper.transform);
+			//slope.transform.position = new Vector3(ch)
+		}
+
+
+	}
+
+}
